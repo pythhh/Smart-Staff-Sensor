070b4407a137620b9724bb8f4d1aa40e7a56ea99
pythhh <41883530+pythhh@users.noreply.github.com>
2019-05-02 22:22:08 +0700

    add comment

--- code/main.ino
+++ code/main.ino
@@ -2,6 +2,7 @@ long distance,duration;
 
 void setup() {
   Serial.begin (9600);
+  //setting pinMode of 2 ultrasonic, buzzer
   pinMode(D1, OUTPUT);
   pinMode(D2, INPUT);
   pinMode(D3, OUTPUT);
@@ -21,6 +22,7 @@ void loop() {
   Serial.print("Distance DOWN:");
   Serial.println(sensor1);
   
+  //condition of active buzzer
   if(sensor1 < 50 || sensor2 < 150){
     digitalWrite(D0, HIGH);
     delay(2);
@@ -34,13 +36,15 @@ void loop() {
   
 }
 void SonarSensor(int trigPinSensor, int echoPinSensor){
+  //clear trigPin
   digitalWrite(trigPinSensor, LOW);
   delayMicroseconds(2);
+  // receive value of trigPin
   digitalWrite(trigPinSensor, HIGH);
   delayMicroseconds(10);
   
   digitalWrite(trigPinSensor, LOW);
-
+  //send it to echoPin & calculate to distance
   duration = pulseIn(echoPinSensor,HIGH);
   distance =(duration/2)/29.1;
 }
